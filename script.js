ab4791a364b11096d3f5b9532e7ca2c292987dcf
Paks176 <paks176@gmail.com>
2022-03-14 20:00:53 +0300

    Added possibility to add tasks, clear all, working on deleting tasks

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
+const emptyMessage = document.querySelector('.empty__message')
+const taskPull = document.querySelector('.tasks__list')
+
 const taskList = {
     pull: [],
     clear() {
         this.pull = [];
+        taskPull.innerHTML = '';
+        this.render()
+    },
+    deleteTask(taskNumber) {
+        this.pull.splice(taskNumber, 1);
+        this.render();
     },
     createTask() {
-        let newTask = new Task('new task');
+        let newTask = new Task();
         this.pull.push(newTask);
+        this.render();
+        taskPull.insertAdjacentHTML('beforeend', `
+        <div class="task">
+                <div class="task__left">
+                    <div class="task__number">${newTask.number}</div>
+                    <input type="checkbox" class="task__checkbox">
+                    <p class="task__text">Lorem ipsum dolor sit <i class="fa-solid fa-pencil"></i>amet consectetur?</p>
+                </div>
+                <div class="task__right">
+                    <button id="edit">
+                        <img src="./icons/pencil-256x256.png" alt="Edit" class="task__button">
+                    </button>
+                    <button id="delete">
+                        <img src="./icons/trash-icon-256.png" alt="Delete" class="task__button">
+                    </button>
+                </div>
+            </div>
+        `)
+        
+    },  
+    render() {
+        if (this.pull.length === 0) {
+            emptyMessage.innerHTML = '<p>Its empty here</p>';
+        }
+        else {
+            emptyMessage.innerHTML = '';
+        }
     }
 };
+
+taskList.render();
 
 class Task {
     marked = false // открытое поле класса
@@ -25,9 +63,10 @@ class Task {
                 break;
         }
     }
-    deleteTask() {
-        return (taskList.pull).splice(this.number, 1);
-    }
+    addListener() {
+        document.querySelector('#delete').addEventListener('click', () => taskList.deleteTask(newTask.number))}
 
 }
 
+const addTask = document.querySelector('#add_task').addEventListener('click', () => taskList.createTask())
+const clearTasks = document.querySelector('#clear_tasks').addEventListener('click', () => taskList.clear())
